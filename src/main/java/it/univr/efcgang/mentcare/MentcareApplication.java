43a18277bf6704ff6557d2f89590442a08968de9
Cesare Montresor <cesare.montresor@gmail.com>
2021-12-16 17:33:44 +0100

    first commit

---
+++ src/main/java/it/univr/efcgang/mentcare/MentcareApplication.java
@@ -0,0 +1,13 @@
+package it.univr.efcgang.mentcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentcareApplication.class, args);
+	}
+
+}
