817a9ec4c7aace236586985197dbd62fde5d6a74
unknown <fabretti.fabiola@gmail.com>
2021-12-21 23:11:48 +0100

    Added controller stub for adding a prescription (waiting for final repository)

--- src/main/java/it/univr/efcgang/mentcare/controller/PrescriptionController.java
+++ src/main/java/it/univr/efcgang/mentcare/controller/PrescriptionController.java
@@ -2,6 +2,8 @@ package it.univr.efcgang.mentcare.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -12,11 +14,16 @@ public class PrescriptionController {
 
         return "prescription/index";
     }
-    @GetMapping("/prescription/create")
-    public String createPrescription() {
 
-        return "prescription/create";
+    @RequestMapping("/create")
+    public String create(
+            @RequestParam(name="patient_id", required=true) long patient_id,
+            @RequestParam(name="drug_id", required=true) long drug_id,
+            @RequestParam(name="dosage",required = true) String dosage){
+
+        // TODO: will save in the repository once we have one
+        // repository.save(new Person(firstname,lastname))
+        return "redirect:/prescription";
     }
-
 
 }
