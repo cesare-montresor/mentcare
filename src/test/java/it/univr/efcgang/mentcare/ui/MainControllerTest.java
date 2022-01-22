e4a20eac4c3b2786d4e632cc9fc2da32afebd502
Cesare Montresor <cesare.montresor@gmail.com>
2022-01-22 17:17:54 +0100

    main controller tests login

--- src/test/java/it/univr/efcgang/mentcare/ui/MainControllerTest.java
+++ src/test/java/it/univr/efcgang/mentcare/ui/MainControllerTest.java
@@ -9,8 +9,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -21,6 +23,15 @@ class MainControllerTest extends BrowserTest {
 	public void testLogin() throws InterruptedException {
 
 		browserLogin("maria", "maria");
+		assertTrue(mainPO.isLoggedIn());
+	}
+
+	@Test
+	public void testLoginProfile() throws InterruptedException {
+
+		browserLogin("maria", "maria");
+		assertTrue(mainPO.isLoggedIn());
+
 		browser.driver.get(url("/profile"));
 
 		String title_txt = mainPO.profileTitle.getText().trim();
@@ -30,8 +41,19 @@ class MainControllerTest extends BrowserTest {
 		assertEquals(title_txt, "Profile");
 		assertEquals(username_txt , "maria");
 		assertEquals(roles_txt, "DOCTOR");
-
 	}
+
+	@Test
+	public void testLoginDoctor() throws InterruptedException {
+
+		browserLogin("maria", "maria");
+		assertTrue(mainPO.isLoggedIn());
+
+		ArrayList<String> names = new ArrayList<>();
+		mainPO.menuItems.forEach( item -> names.add( item.getText() ) );
+		System.out.println(names);
+	}
+
 
 	@Test
 	public void testWelcome() {
